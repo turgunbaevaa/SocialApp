7788cadd243fd5817cd4b07c22fc17995cf6a49a
turgunbaevaa <nyc.aruu@gmail.com>
2024-01-12 15:07:34 +0100

    Initial Commit

---
+++ SocialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Aruuke Turgunbaeva on 12/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
